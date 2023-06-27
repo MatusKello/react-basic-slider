50f9cd25b8643bf0ec2658a7cbce8030a7a0966c
Matus Kello <m.kello1992@gmail.com>
2023-06-27 17:31:06 +0200

    initial commit

---
+++ src/components/OneMovieSlider.jsx
@@ -0,0 +1,41 @@
+import './OneMovieSlider.css';
+import data from '../data';
+import { useState } from 'react';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+
+const OneMovieSlider = () => {
+  const [index, setIndex] = useState(0);
+  const { image, title, description } = data[index];
+
+  const handlePrevious = () => {
+    if (index === 0) {
+      setIndex(data.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (index === data.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  return (
+    <div>
+      <img src={image} alt='' />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <button onClick={handlePrevious}>
+        <FaArrowAltCircleLeft />
+      </button>
+      <button onClick={handleNext}>
+        <FaArrowAltCircleRight />
+      </button>
+    </div>
+  );
+};
+
+export default OneMovieSlider;
